233d9384e3646c6130315ff8dee564e48623724f
hamid <heslami32@gmail.com>
2021-08-17 13:16:24 +0430

    change: comment change

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -24,7 +24,7 @@ const PostsPage = () => {
   );
 };
 
-// Map Rrdux state to React component props and ...
+// Map redux state to React component props and ...
 
 // const mapStateToProps = (state) => ({
 //   loading: state.posts.loading,
